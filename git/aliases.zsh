c4854fa519c1cac4b5bca0958a3c7975795f595c
Daan van Dijk <github@drvd.nl>
2016-12-09 11:36:28 +0100

    Update prezto (fix GREP_OPTIONS), reorder git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,9 @@
+alias gs='git status'
+alias gb='git branch'
+alias go='git checkout'
+alias gh='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+#alias gh='git hist'
+
 alias gd='git diff'
 alias gdc='git diff --cached'
-alias gh='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
-alias go='git checkout'
-alias gs='git status'
 alias gci='git checkout -- .idea'
