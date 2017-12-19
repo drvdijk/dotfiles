aec39001f2e7b154237a5c9c9f256f7aa963d0dd
Daan van Dijk <github@drvd.nl>
2017-12-19 15:50:24 +0100

    Install mas apps

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,9 +2,10 @@
 #
 # homebrew and cask
 #
-# calls homebrew script first, followed by the caskroom script
+# calls homebrew script first, followed by the caskroom script, followed by mac app store apps
 # reorganizing the dotfiles, this will change (soon?) to just use a Brewfile followed
 # by some app-specific stuff like the current homebrew/caskroom scripts currently do
 
 source $(dirname ${BASH_SOURCE[0]})/homebrew.sh
 source $(dirname ${BASH_SOURCE[0]})/cask.sh
+source $(dirname ${BASH_SOURCE[0]})/mas.sh
