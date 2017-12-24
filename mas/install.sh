46890b6adb58d6c1d6a07c66df982b20e67761c3
Daan van Dijk <github@drvd.nl>
2017-12-24 23:58:10 +0100

    Add Airmail, disable slack (for now)

--- mas/install.sh
+++ mas/install.sh
@@ -21,9 +21,10 @@ require_brew mas
 # Apps!
 mas install 511324989 #JSON Accelerator (1.1.1)
 mas install 411643860 #DaisyDisk (4.4)
-mas install 803453959 #Slack (3.0.0)
+#mas install 803453959 #Slack (3.0.0) # maybe Franz is good enough
 mas install 442160987 #Flycut (1.5)
 mas install 410628904 #Wunderlist (3.4.7)
+mas install 918858936 #Airmail 3 (3.5.4)
 
 bot "All done with app store apps!"
 
