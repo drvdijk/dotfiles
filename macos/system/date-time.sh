2d7f973ef0d0ac3f5041ac2804be71500c5c21f0
Daan van Dijk <github@drvd.nl>
2017-12-28 23:05:49 +0100

    Some more app/default settings

--- macos/system/date-time.sh
+++ macos/system/date-time.sh
@@ -7,15 +7,15 @@
 # Set the timezone; see `systemsetup -listtimezones` for other values
 sudo systemsetup -settimezone "Europe/Amsterdam" > /dev/null
 
-# # Set date and time automatically
-# sudo systemsetup -setusingnetworktime on > /dev/null
-#
-# # Set time server
-# sudo systemsetup -setnetworktimeserver "time.apple.com" > /dev/null
-#
-# # Set time zome automatically using current location
-# sudo defaults write /Library/Preferences/com.apple.timezone.auto.plist Active -bool true
-#
+# Set date and time automatically
+sudo systemsetup -setusingnetworktime on > /dev/null
+
+# Set time server
+sudo systemsetup -setnetworktimeserver "time.apple.com" > /dev/null
+
+# Set time zome automatically using current location
+sudo defaults write /Library/Preferences/com.apple.timezone.auto.plist Active -bool true
+
 # # Menu bar clock format
 # # "h:mm" Default
 # # "HH"   Use a 24-hour clock
