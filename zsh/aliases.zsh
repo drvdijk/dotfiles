6d62637aad5427419bb8afff1f2117cbf663a39e
Daan van Dijk <daan@drvdijk.nl>
2018-02-26 14:08:41 +0100

    Switch to using zplug instead of having zprezto repo as submodule
    The idea is to be able to quickly try other plugins, clean up my repo a
    bit, and get a more up to date terminal. However, there seem to be quite
    some speed issues. This is why there's also some antigen and zgen
    prototyping in the zshrc file. For now let's see how this works.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-alias ls="${aliases[ls]:-ls} -aF"
+alias ls="${aliases[ls]:-ls} -aFG"
 
 alias sweep="find . -name .DS_Store -type f -delete ; find . -type d -print0 | xargs -0 dot_clean -m"
 
