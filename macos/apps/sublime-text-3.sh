47181ada21d58ac16b88e32da254c224d7b53479
Daan van Dijk <github@drvd.nl>
2019-07-18 19:08:30 +0200

    Fixes, java8 / mkdir

--- macos/apps/sublime-text-3.sh
+++ macos/apps/sublime-text-3.sh
@@ -5,4 +5,5 @@
 ###############################################################################
 
 mkdir -p ~/Dropbox/sync/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 ln -s ~/Dropbox/sync/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
