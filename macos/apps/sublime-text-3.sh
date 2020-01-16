9c5cd1338725d11b4ec4397e786b2f55e875ba97
Daan van Dijk <github@drvd.nl>
2020-01-16 15:31:05 +0100

    Prevent quit on benign error
    Don't stop installing stuff if quitting apps or linking prefs doesn't work

--- macos/apps/sublime-text-3.sh
+++ macos/apps/sublime-text-3.sh
@@ -6,4 +6,4 @@
 
 mkdir -p ~/Dropbox/sync/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-ln -s ~/Dropbox/sync/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -s ~/Dropbox/sync/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User || true
