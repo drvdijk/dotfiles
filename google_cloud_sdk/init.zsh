8c8f2ad3d5e8fbdd2f9932cb32381905c54ddba7
Daan van Dijk <github@drvd.nl>
2021-04-28 22:04:17 +0200

    Development stuff, mostly

---
+++ google_cloud_sdk/init.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+[[ -s "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+[[ -s "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
