1352a3ee666f38e99b973602777b0314cffc2d92
Daan van Dijk <daan@drvdijk.nl>
2024-11-09 21:58:34 +0100

    Fix broken sublime text packages folder, Dropbox linked by Mountain Duck

---
+++ macos/apps/mountain-duck.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Mountain Duck
+###############################################################################
+
+ln -s /Users/drvdijk/Library/Group\ Containers/G69SCX94XU.duck/Library/Application\ Support/duck/Volumes.noindex/Dropbox ~/Dropbox
--- macos/apps/sublime-text-3.sh
+++ macos/apps/sublime-text-3.sh
@@ -4,6 +4,5 @@
 # Sublime Text 3
 ###############################################################################
 
-mkdir -p ~/Dropbox/sync/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 ln -s ~/Dropbox/sync/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User || true
