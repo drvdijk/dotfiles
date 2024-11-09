d80308bb970dc03c713568d4c1718837d100f179
Daan van Dijk <github@drvd.nl>
2024-11-09 21:58:34 +0100

    Fix broken sublime text packages folder, Dropbox linked by Mountain Duck

--- macos/install.sh
+++ macos/install.sh
@@ -160,6 +160,7 @@ set_prefs textedit "TextEdit"
 # set_prefs flycut "Flycut"
 set_prefs google-chrome "Google Chrome"
 set_prefs iterm "iTerm"
+set_prefs mountain-duck "Mountain Duck"
 # set_prefs sizeup "SizeUp"
 set_prefs sublime-text-3 "Sublime Text"
 
