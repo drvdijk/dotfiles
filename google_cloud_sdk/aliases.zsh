fb21a415a2875a7bed7086e27852a093f08ae95d
Daan van Dijk <github@drvd.nl>
2025-01-06 08:43:18 +0100

    Add gcloud aliases and ssh speedup

--- google_cloud_sdk/aliases.zsh
+++ google_cloud_sdk/aliases.zsh
@@ -1,1 +1,2 @@
 alias gssh='gcloud compute ssh --tunnel-through-iap'
+alias gscp='gcloud compute scp --tunnel-through-iap'
--- google_cloud_sdk/init.zsh
+++ google_cloud_sdk/init.zsh
@@ -4,3 +4,7 @@ if [ -x "$(command -v brew)" ]; then
 	[[ -s "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 	[[ -s "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 fi
+
+if $(gcloud info --format="value(basic.python_location)") -m pip show numpy > /dev/null 2>&1; then
+    export CLOUDSDK_PYTHON_SITEPACKAGES=1
+fi
