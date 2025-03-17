506089b31e61837945a2496bc82455ac2668f048
Daan van Dijk <github@drvd.nl>
2025-03-17 13:38:11 +0100

    Disable contacts settings for now
    Something to do with full disk access?

--- macos/apps/contacts.sh
+++ macos/apps/contacts.sh
@@ -1,52 +1,52 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-###############################################################################
-# Contacts
-###############################################################################
+# ###############################################################################
+# # Contacts
+# ###############################################################################
 
-# Enable the debug menu in Address Book
-#defaults write com.apple.AddressBook ABShowDebugMenu -bool true
+# # Enable the debug menu in Address Book
+# #defaults write com.apple.AddressBook ABShowDebugMenu -bool true
 
-# Show first name
-# false : Before last name
-# true  : Following last name
-defaults write com.apple.AddressBook ABNameDisplay -bool false
+# # Show first name
+# # false : Before last name
+# # true  : Following last name
+# defaults write com.apple.AddressBook ABNameDisplay -bool false
 
-# Sort by
-defaults write com.apple.AddressBook ABNameSortingFormat -string "sortingFirstName sortingLastName"
+# # Sort by
+# defaults write com.apple.AddressBook ABNameSortingFormat -string "sortingFirstName sortingLastName"
 
-# Short name format
-# 0: Full Name
-# 1: First Name & Last Initial
-# 2: First Initial & Last Name
-# 3: First Name Only
-# 4: Last Name Only
-defaults write com.apple.AddressBook ABShortNameStyle -int 2
+# # Short name format
+# # 0: Full Name
+# # 1: First Name & Last Initial
+# # 2: First Initial & Last Name
+# # 3: First Name Only
+# # 4: Last Name Only
+# defaults write com.apple.AddressBook ABShortNameStyle -int 2
 
-# Prefer nicknames
-defaults write com.apple.AddressBook ABShortNamePrefersNickname -bool true
+# # Prefer nicknames
+# defaults write com.apple.AddressBook ABShortNamePrefersNickname -bool true
 
-# Address format
-defaults write com.apple.AddressBook ABDefaultAddressCountryCode -string "nl"
+# # Address format
+# defaults write com.apple.AddressBook ABDefaultAddressCountryCode -string "nl"
 
-# vCard Format
-# false: 3.0
-# true : 2.1
-defaults write com.apple.AddressBook ABUse21vCardFormat -bool false
+# # vCard Format
+# # false: 3.0
+# # true : 2.1
+# defaults write com.apple.AddressBook ABUse21vCardFormat -bool false
 
-# Enable private me card
-defaults write com.apple.AddressBook ABPrivateVCardFieldsEnabled -bool false
+# # Enable private me card
+# defaults write com.apple.AddressBook ABPrivateVCardFieldsEnabled -bool false
 
-# Export notes in vCards
-defaults write com.apple.AddressBook ABIncludeNotesInVCard -bool false
+# # Export notes in vCards
+# defaults write com.apple.AddressBook ABIncludeNotesInVCard -bool false
 
-# Export photos in vCards
-defaults write com.apple.AddressBook ABIncludePhotosInVCard -bool false
+# # Export photos in vCards
+# defaults write com.apple.AddressBook ABIncludePhotosInVCard -bool false
 
-# Show first name:
-# 1: Before last name
-# 2: Following last name
-defaults write NSGlobalDomain NSPersonNameDefaultDisplayNameOrder -int 1
+# # Show first name:
+# # 1: Before last name
+# # 2: Following last name
+# defaults write NSGlobalDomain NSPersonNameDefaultDisplayNameOrder -int 1
 
-# Prefer nicknames
-defaults write NSGlobalDomain NSPersonNameDefaultShouldPreferNicknamesPreference -bool true
+# # Prefer nicknames
+# defaults write NSGlobalDomain NSPersonNameDefaultShouldPreferNicknamesPreference -bool true
