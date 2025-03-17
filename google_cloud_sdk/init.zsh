ebb4a68cb890103a0839339cc4bec8613c89f0c1
Daan van Dijk <github@drvd.nl>
2025-03-17 13:34:03 +0100

    Check if gcloud is installed

--- google_cloud_sdk/init.zsh
+++ google_cloud_sdk/init.zsh
@@ -5,6 +5,8 @@ if [ -x "$(command -v brew)" ]; then
 	[[ -s "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 fi
 
-if $(gcloud info --format="value(basic.python_location)") -m pip show numpy > /dev/null 2>&1; then
-    export CLOUDSDK_PYTHON_SITEPACKAGES=1
+if [ -x "$(command -v gcloud)" ]; then
+	if $(gcloud info --format="value(basic.python_location)") -m pip show numpy > /dev/null 2>&1; then
+	    export CLOUDSDK_PYTHON_SITEPACKAGES=1
+	fi
 fi
