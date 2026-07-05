c90c0c23a8bd1bf7bf5f3c5a693fd3a22eaf0a05
Daan van Dijk <github@drvd.nl>
2026-07-05 13:54:25 +0200

    Fix asdf path

--- asdf/init.zsh
+++ asdf/init.zsh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
 [[ -s "/opt/homebrew/opt/asdf/libexec/asdf.sh" ]] && source "/opt/homebrew/opt/asdf/libexec/asdf.sh"
+
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+
