bda64de2ac70316bac90b19d3e0306e4754bf17c
Daan van Dijk <github@drvd.nl>
2026-07-06 13:03:05 +0200

    Fix copy-paste bug in google_cloud_sdk/init.zsh

--- google_cloud_sdk/init.zsh
+++ google_cloud_sdk/init.zsh
@@ -2,7 +2,7 @@
 
 if [ -x "$(command -v brew)" ]; then 
 	[[ -s "$(brew --prefix)/Caskroom/gcloud-cli/latest/google-cloud-sdk/path.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/gcloud-cli/latest/google-cloud-sdk/path.zsh.inc"
-	[[ -s "$(brew --prefix)/Caskroom/gcloud-cli/latest/google-cloud-sdk/path.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/gcloud-cli/latest/google-cloud-sdk/completion.zsh.inc"
+	[[ -s "$(brew --prefix)/Caskroom/gcloud-cli/latest/google-cloud-sdk/completion.zsh.inc" ]] && source "$(brew --prefix)/Caskroom/gcloud-cli/latest/google-cloud-sdk/completion.zsh.inc"
 fi
 
 if [ -x "$(command -v gcloud)" ]; then
