60c612bd381f18628f840b1275d914ef24d6a203
Daan van Dijk <github@drvd.nl>
2026-09-14 12:26:41 +0200

    Make antidote startup script do some sanity checks

--- zsh/antidote.sh
+++ zsh/antidote.sh
@@ -6,11 +6,15 @@ zsh_plugins="$DOTFILES/zsh/zsh_plugins.txt"
 zsh_plugins_static="$DOTFILES/zsh/zsh_plugins.zsh"
 
 if [[ ! -s "$zsh_plugins_static" || "$zsh_plugins" -nt "$zsh_plugins_static" ]]; then
-  source "$(brew --prefix)/opt/antidote/share/antidote/antidote.zsh"
-  antidote bundle <"$zsh_plugins" >"$zsh_plugins_static"
+  if command -v brew >/dev/null 2>&1 && [[ -s "$(brew --prefix)/opt/antidote/share/antidote/antidote.zsh" ]]; then
+    source "$(brew --prefix)/opt/antidote/share/antidote/antidote.zsh"
+    antidote bundle <"$zsh_plugins" >"$zsh_plugins_static"
+  else
+    print -u2 "antidote: homebrew/antidote not installed yet, skipping plugin bundle (run \`dotfiles install homebrew\`)"
+  fi
 fi
 
-source "$zsh_plugins_static"
+[[ -s "$zsh_plugins_static" ]] && source "$zsh_plugins_static"
 
 # zsh-completions only extends fpath; compinit isn't called for us.
 autoload -Uz compinit && compinit
